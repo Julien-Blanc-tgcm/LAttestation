10c5ce04adc9d3e587204a63f43c87eff5457893
Julien Blanc <julien.blanc@tgcm.eu>
2020-11-08 07:58:05 +0100

    Added gitignore file and changes files
    Also added translations, even if not used

---
+++ translations/harbour-lattestation.ts
@@ -0,0 +1,165 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>Access</name>
+    <message>
+        <location filename="../qml/pages/Access.qml" line="13"/>
+        <source>Attestations en cours</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Access.qml" line="24"/>
+        <source>Effacer</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <location filename="../qml/cover/CoverPage.qml" line="8"/>
+        <source>Attestation de sortie</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Generate</name>
+    <message>
+        <location filename="../qml/pages/Generate.qml" line="9"/>
+        <source>Agrandir la plage horaire</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Generate.qml" line="26"/>
+        <source>Contenu</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Generate.qml" line="31"/>
+        <source>Motif</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Generate.qml" line="33"/>
+        <source>Courses</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Generate.qml" line="34"/>
+        <source>Loisir</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Generate.qml" line="35"/>
+        <source>Déplacement pro</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Generate.qml" line="36"/>
+        <source>Consultation médicale</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Generate.qml" line="37"/>
+        <source>Assistance pers. vulnérable</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Generate.qml" line="38"/>
+        <source>Mission intérêt public</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Generate.qml" line="39"/>
+        <source>Accompagnement enfants</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Generate.qml" line="40"/>
+        <source>Convocation</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Generate.qml" line="41"/>
+        <source>Aide handicapé</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Generate.qml" line="47"/>
+        <location filename="../qml/pages/Generate.qml" line="48"/>
+        <source>Fait à</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Generate.qml" line="52"/>
+        <source>Heure de sortie</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Generate.qml" line="62"/>
+        <source>Générer</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>HomePage</name>
+    <message>
+        <location filename="../qml/pages/HomePage.qml" line="16"/>
+        <source>Attestation de sortie</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/HomePage.qml" line="21"/>
+        <source>Paramètres</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/HomePage.qml" line="28"/>
+        <source>Générer une attestation</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/HomePage.qml" line="32"/>
+        <source>Accéder à une attestation</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="21"/>
+        <source>Paramètres</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="26"/>
+        <source>Prénom</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="35"/>
+        <source>Nom</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="44"/>
+        <source>Date de naissance</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="53"/>
+        <source>Lieu de naissance</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="62"/>
+        <source>Adresse</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="71"/>
+        <source>Valeur par défaut pour le « fait à »</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
